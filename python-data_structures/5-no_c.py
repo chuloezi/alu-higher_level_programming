14e838f5641fcb77a70a0f152219c6c0f97522ad
chuloezi <e.munyengan@alustudent.com>
2026-03-30 12:45:41 -0700

    modifications on 4-new_in_list.py and 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # Create an empty string to store our result
     new_string = ""
-    
-    # Iterate through every character in the original string
     for char in my_string:
-        # Check if the character is NOT 'c' and NOT 'C'
         if char != 'c' and char != 'C':
-            # Add the character to our new string
             new_string += char
-            
     return new_string
